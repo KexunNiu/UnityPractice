c6c19a59a66756167ee09314dc15ba481f4122dd
KN <kexun@ualberta.ca>
2023-08-01 16:51:25 -0600

    Update Scorer.cs Dropper.cs

---
+++ Obstacle/Assets/Script/Dropper.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dropper : MonoBehaviour
+{      
+    MeshRenderer render;
+    Rigidbody rigid;
+    [SerializeField] float timeToWait = 1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        render = GetComponent<MeshRenderer>();
+        rigid = GetComponent<Rigidbody>();
+
+        render.enabled = false;
+        rigid.useGravity = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.time == timeToWait)
+        {   
+            render.enabled = true;
+            rigid.useGravity = true;
+        }
+    }
+}
--- Obstacle/Assets/Script/ObjectHit.cs
+++ Obstacle/Assets/Script/ObjectHit.cs
@@ -3,12 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ObjectHit : MonoBehaviour
-{
+{   
     private void OnCollisionEnter(Collision other) 
     {
-        Debug.Log("Bumped into a wall");
-
+        if (other.gameObject.tag == "Player")
+        {
+            GetComponent<MeshRenderer>().material.color = Color.red;
+            gameObject.tag = "Hit";
+        }
         
         
     }
+
+    
 }
